26c008d44605888f9aa02aad7be7a2e6ae876178
Punisher <singhgurpreet1212@gmail.com>
2021-01-10 04:20:48 +0530

    expand and collapse the accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -4,28 +4,25 @@ const Accordion = ({ items }) => {
   const [activeIndex, setActiveIndex] = useState(null);
 
   const onTitleClick = (index) => {
-    setActiveIndex(index);
+    setActiveIndex(index === activeIndex ? null : index);
   };
 
-  const renderedItems = items.map((item, i) => {
+  const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
+
     return (
       <React.Fragment key={item.title}>
-        <div className="title active" onClick={() => onTitleClick(i)}>
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
           <i className="dropdown icon" />
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
   });
-  return (
-    <div className="ui styled accordion">
-      {renderedItems}
-      <h1>{activeIndex}</h1>
-    </div>
-  );
+  return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
 export default Accordion;
