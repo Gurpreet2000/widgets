8cc6ae0d89faae3bded7014c9a74f506feee3fb1
Punisher <singhgurpreet1212@gmail.com>
2021-01-11 03:55:59 +0530

    fix empty search term error

--- src/components/Search.js
+++ src/components/Search.js
@@ -22,12 +22,20 @@ const Search = () => {
       setResults(data.query.search);
     };
 
-    search();
+    if (term) search();
   }, [term]);
 
   const renderedResults = results.map((result) => {
     return (
       <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
         <div className="content">
           <div className="header">{result.title}</div>
           <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
